5bb4b880851e095b335cc5fab7815a8d3ae40ead
Csaba Gyuris <gycsaba@vasvari.hu>
2022-03-02 09:12:16 +0100

    Feladat üres fájlokkal és teszt adatokkal

---
+++ Kreta/ViewModel/ClassViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Kreta.Models;
+using Kreta.Repositories;
+using System.Collections.ObjectModel;
+
+namespace Kreta.ViewModel
+{
+    public class ClassViewModel
+    {
+        private ClassesRepo classesRepo;
+        private ObservableCollection<Class> classes;
+
+        public ClassViewModel()
+        {
+            classesRepo = new ClassesRepo();
+        }
+
+        public ObservableCollection<Class> Classes { get => classes; set => classes = value; }
+    }
+}
